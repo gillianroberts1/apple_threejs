458febfffcbadacf7507278ffc55b3de06995605
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2024-03-18 14:58:23 +0000

    slider working

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,12 +1,12 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { rightImg, watchImg } from '../utils/index';
+import { rightImg, watchImg } from "../utils/index";
 import VideoCarousel from "./VideoCarousel";
 
 export const Highlights = () => {
   useGSAP(() => {
     gsap.to("#title", { opacity: 1, y: 0 });
-    gsap.to('.link', {opacity: 1, duratio: 1, stagger: 0.25 })
+    gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
   }, []);
 
   return (
@@ -20,17 +20,17 @@ export const Highlights = () => {
             Get the Highlights.
           </h1>
           <div className="flex flex-wrap items-end gap-5">
-            <p className="link">Watch the film
-            <img src={watchImg} alt="watch" className="ml-2"/>
-            
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2" />
             </p>
-            <p className="link">Watch the event
-            <img src={rightImg } alt="right" className="ml-2"/>
-            
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="right" className="ml-2" />
             </p>
           </div>
         </div>
-        <VideoCarousel/>
+        <VideoCarousel />
       </div>
     </section>
   );
